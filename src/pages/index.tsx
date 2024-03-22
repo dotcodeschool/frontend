bbe062345232535ec89a9ef31d0c54623aa0d6a8
batman <iammasterbrucewayne@protonmail.com>
2024-03-22 09:51:17 +0530

    extract header and update CTA button styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,37 +14,46 @@ export default function Index() {
   return (
     <Box maxW="6xl" mx="auto" px={[4, 12]}>
       <Navbar />
-      <Box mt={20}>
-        <Heading as="h1" fontWeight="800" size="4xl" maxW="3xl">
-          Learn to Code Web3 Apps by Building Real Projects.
-        </Heading>
-        <Text mt={4} fontSize="xl" maxW="3xl">
-          Dot Code School is an interactive online school that teaches you how
-          to build meaningful web3 applications using the Polkadot SDK. Learn
-          how to build your own custom blockchain from zero to one hundred.
-        </Text>
-        <ButtonGroup mt={12} spacing={4}>
-          <PrimaryButton
-            as={Link}
-            href="/courses"
-            size="lg"
-            _hover={{ textDecor: "none" }}
-          >
-            Browse Courses
-          </PrimaryButton>
-          <Button
-            as={Link}
-            href="https://forms.gle/2o4hBauCJ6Fkf9Zz7"
-            size="lg"
-            variant="outline"
-            _hover={{ textDecor: "none" }}
-            isExternal
-          >
-            Become a Teacher
-          </Button>
-        </ButtonGroup>
-      </Box>
+      <Header />
       <Footer />
+    </Box>
+  );
+}
+
+function Header() {
+  return (
+    <Box mt={20}>
+      <Heading as="h1" fontWeight="800" size="4xl" maxW="3xl">
+        Learn to Code Web3 Apps by Building Real Projects.
+      </Heading>
+      <Text mt={4} fontSize="xl" maxW="3xl">
+        Dot Code School is an interactive online school that teaches you how to
+        build meaningful web3 applications using the Polkadot SDK. Learn how to
+        build your own custom blockchain from zero to one hundred.
+      </Text>
+      <ButtonGroup mt={12} spacing={4}>
+        <PrimaryButton
+          as={Link}
+          href="/courses"
+          p={8}
+          fontSize={"xl"}
+          _hover={{ textDecor: "none" }}
+        >
+          Browse Courses
+        </PrimaryButton>
+        <Button
+          as={Link}
+          href="https://forms.gle/2o4hBauCJ6Fkf9Zz7"
+          variant="solid"
+          colorScheme="gray"
+          fontSize={"xl"}
+          p={8}
+          _hover={{ textDecor: "none" }}
+          isExternal
+        >
+          Become a Teacher
+        </Button>
+      </ButtonGroup>
     </Box>
   );
 }
