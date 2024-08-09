dc757b70e20c172eaaeffb7db2b3b3f416cc2a14
batman <iammasterbrucewayne@protonmail.com>
2024-08-09 14:29:14 +0530

    add text in place of editor when no files have changed between steps

--- src/components/lessons-interface/editor-tabs.tsx
+++ src/components/lessons-interface/editor-tabs.tsx
@@ -8,9 +8,10 @@ import {
   Flex,
   Tooltip,
   CloseButton,
+  Text,
 } from "@chakra-ui/react";
 import { DiffEditor, Editor } from "@monaco-editor/react";
-import { map, find, matches, endsWith } from "lodash";
+import { map, find, matches, endsWith, isEmpty } from "lodash";
 import stripComments from "strip-comments";
 import { FiMaximize2, FiMinimize2 } from "react-icons/fi";
 import { IoGitCompareOutline } from "react-icons/io5";
@@ -88,60 +89,70 @@ const EditorTabs = ({
             ":hover::-webkit-scrollbar-thumb": { background: "whiteAlpha.300" },
           }}
         >
-          {map(editorContent, (file, i) => {
-            const incorrectColor = find(
-              incorrectFiles,
-              matches({ fileName: file.fileName }),
-            )
-              ? "red.300"
-              : null;
-            return (
-              <Tab
-                key={i}
-                as={
-                  showDiff && endsWith(file.fileName, ".diff") ? "i" : undefined
-                }
-                display={
-                  !showDiff && endsWith(file.fileName, ".diff")
-                    ? "none"
-                    : "block"
-                }
-                borderRight="1px solid"
-                borderRightColor="#111111"
-                color={incorrectColor ? incorrectColor : "whiteAlpha.600"}
-                _hover={{ bg: "whiteAlpha.300" }}
-                _selected={{
-                  bg: "whiteAlpha.100",
-                  color: incorrectColor ? incorrectColor : "orange.200",
-                  borderBottom: "1px solid",
-                  borderColor: incorrectColor ? incorrectColor : "orange.200",
-                  borderRightColor: "#111111",
-                }}
-              >
-                {file.fileName}
-                {showDiff && endsWith(file.fileName, ".diff") && (
-                  <>
-                    &nbsp;(Prev. Changes)
-                    <CloseButton
-                      display={"inline"}
-                      size="sm"
-                      p={0}
-                      verticalAlign="middle"
-                      h="fit-content"
-                      bg="transparent"
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        setShowDiff(false);
-                        setTabIndex(0);
-                      }}
-                    />
-                  </>
-                )}
-              </Tab>
-            );
-          })}
+          {isEmpty(editorContent) ? (
+            <Text m={4}>No files edited in this step.</Text>
+          ) : (
+            map(editorContent, (file, i) => {
+              const incorrectColor = find(
+                incorrectFiles,
+                matches({ fileName: file.fileName }),
+              )
+                ? "red.300"
+                : null;
+              return (
+                <Tab
+                  key={i}
+                  as={
+                    showDiff && endsWith(file.fileName, ".diff")
+                      ? "i"
+                      : undefined
+                  }
+                  display={
+                    !showDiff && endsWith(file.fileName, ".diff")
+                      ? "none"
+                      : "block"
+                  }
+                  borderRight="1px solid"
+                  borderRightColor="#111111"
+                  color={incorrectColor ? incorrectColor : "whiteAlpha.600"}
+                  _hover={{ bg: "whiteAlpha.300" }}
+                  _selected={{
+                    bg: "whiteAlpha.100",
+                    color: incorrectColor ? incorrectColor : "orange.200",
+                    borderBottom: "1px solid",
+                    borderColor: incorrectColor ? incorrectColor : "orange.200",
+                    borderRightColor: "#111111",
+                  }}
+                >
+                  {file.fileName}
+                  {showDiff && endsWith(file.fileName, ".diff") && (
+                    <>
+                      &nbsp;(Prev. Changes)
+                      <CloseButton
+                        display={"inline"}
+                        size="sm"
+                        p={0}
+                        verticalAlign="middle"
+                        h="fit-content"
+                        bg="transparent"
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          setShowDiff(false);
+                          setTabIndex(0);
+                        }}
+                      />
+                    </>
+                  )}
+                </Tab>
+              );
+            })
+          )}
         </Flex>
-        <Flex alignItems="center" px={2}>
+        <Flex
+          alignItems="center"
+          px={2}
+          display={isEmpty(editorContent) ? "none" : "flex"}
+        >
           <Tooltip
             hasArrow
             maxW={32}
