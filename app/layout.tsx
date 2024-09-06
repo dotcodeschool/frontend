fba53c804e7a36f94d19ff857bc32e1bf74170c2
batman <iammasterbrucewayne@protonmail.com>
2024-09-06 15:01:27 +0530

    add vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,19 @@
 import { Providers } from "@/app/providers";
 import { ColorModeScript } from "@chakra-ui/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import theme from "@/app/ui/theme";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Providers>{children}</Providers>
+        <SpeedInsights />
       </body>
     </html>
   );
