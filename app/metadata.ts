a74b0e4fe939e3e2327f381e7e1b64687de17754
batman <iammasterbrucewayne@protonmail.com>
2024-09-17 13:55:51 +0530

    fix(app/metadata): remove redundant async

--- app/metadata.ts
+++ app/metadata.ts
@@ -1,4 +1,4 @@
-export const generateMetadata = async () => {
+export const generateMetadata = () => {
   const title = `Dot Code School | Learn Blockchain & Web3 Development Fast`;
   const description = `Learn to build web3 applications and custom blockchains using the Polkadot SDK. Master blockchain development through hands-on learning with our interactive courses!`;
 
