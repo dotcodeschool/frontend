cff93f85a2fa2a48bcaa0891c03634a00073e88b
batman <iammasterbrucewayne@protonmail.com>
2024-09-17 12:56:40 +0530

    fix(app/courses/page): update page to use CourseModule type

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -3,15 +3,19 @@ import { IoArrowBack } from "react-icons/io5";
 
 import { ButtonPrimary, Navbar } from "@/components";
 import { QUERY_COURSE_CATALOG, fetchGraphQL } from "@/lib/api";
-import { TypeCourseModuleFields } from "@/lib/types/contentful";
+import { CourseModule } from "@/lib/types";
 
 export { generateMetadata } from "./metadata";
 
 const CoursesPage = async () => {
-  const courses = await fetchGraphQL<Array<TypeCourseModuleFields>>(
-    QUERY_COURSE_CATALOG,
-    "courseModuleCollection",
-  );
+  const courses = await fetchGraphQL<
+    Array<
+      Pick<
+        CourseModule,
+        "slug" | "title" | "level" | "language" | "description"
+      >
+    >
+  >(QUERY_COURSE_CATALOG, "courseModuleCollection");
 
   return (
     <Box maxW="8xl" mx="auto" px={[4, 12]}>
@@ -23,26 +27,26 @@ const CoursesPage = async () => {
         <Heading as="h1" fontWeight="800" my={4} size="xl">
           Courses
         </Heading>
-        {courses.map((course) => (
+        {courses.map(({ slug, title, description, level, language }) => (
           <Card
             direction={{ base: "column", md: "row" }}
-            key={course.slug}
+            key={slug}
             my={4}
             overflow="hidden"
             p={8}
           >
             <Stack>
               <Heading as="h2" size="md">
-                {course.title}
+                {title}
               </Heading>
               <Text>
-                {course.level} • {course.language}
+                {level} • {language}
               </Text>
-              <Text py={2}>{course.description}</Text>
+              <Text py={2}>{description}</Text>
               <ButtonPrimary
                 _hover={{ textDecor: "none" }}
                 as={Link}
-                href={`/courses/${course.slug}`}
+                href={`/courses/${slug}`}
                 mt={4}
                 w="fit-content"
               >
