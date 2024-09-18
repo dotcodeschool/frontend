36dfba660a7fd65d96dc3d9376d3b960d169e393
batman <iammasterbrucewayne@protonmail.com>
2024-09-18 11:35:18 +0530

    WIP(app/courses/[course]/page): saving progress

--- app/courses/[course]/page.tsx
+++ app/courses/[course]/page.tsx
@@ -2,31 +2,26 @@ import { Box, Link } from "@chakra-ui/react";
 import React, { Suspense } from "react";
 import { IoArrowBack } from "react-icons/io5";
 
-import Navbar from "@/components/navbar";
-import { TypeCourseModuleFields } from "@/lib/types/contentful";
-import { getCourseData } from "@/lib/utils";
-
-import CourseContent from "./components/CourseContent";
+import { Navbar } from "@/components";
+import { CourseContent } from "./components/CourseContent";
 import CourseContentSkeleton from "./components/CourseContentSkeleton";
 
 export { generateMetadata } from "./metadata";
 
-async function CoursePage({ params }: { params: { course: string } }) {
-  const courseData: TypeCourseModuleFields = await getCourseData(params.course);
-
+const CoursePage = ({ params: { course } }: { params: { course: string } }) => {
   return (
     <Box maxW="8xl" mx="auto" px={[4, 12]}>
       <Navbar cta={false} />
       <Box maxW="4xl" mx="auto">
-        <Link href="/courses" color="green.500" fontSize="5xl">
+        <Link color="green.500" fontSize="5xl" href="/courses">
           <IoArrowBack />
         </Link>
         <Suspense fallback={<CourseContentSkeleton />}>
-          <CourseContent {...courseData} />
+        <CourseContent slug={course} />
         </Suspense>
       </Box>
     </Box>
   );
-}
+};
 
 export default CoursePage;
