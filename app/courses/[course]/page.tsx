db220e23cd94a8c9c98073a02a471744570d4da4
batman <iammasterbrucewayne@protonmail.com>
2024-09-18 16:26:59 +0530

    refactor(app/courses/[course]/{metadata,page}): use helper functions

--- app/courses/[course]/page.tsx
+++ app/courses/[course]/page.tsx
@@ -3,25 +3,24 @@ import React, { Suspense } from "react";
 import { IoArrowBack } from "react-icons/io5";
 
 import { Navbar } from "@/components";
+
 import { CourseContent } from "./components/CourseContent";
 import CourseContentSkeleton from "./components/CourseContentSkeleton";
 
 export { generateMetadata } from "./metadata";
 
-const CoursePage = ({ params: { course } }: { params: { course: string } }) => {
-  return (
-    <Box maxW="8xl" mx="auto" px={[4, 12]}>
-      <Navbar cta={false} />
-      <Box maxW="4xl" mx="auto">
-        <Link color="green.500" fontSize="5xl" href="/courses">
-          <IoArrowBack />
-        </Link>
-        <Suspense fallback={<CourseContentSkeleton />}>
+const CoursePage = ({ params: { course } }: { params: { course: string } }) => (
+  <Box maxW="8xl" mx="auto" px={[4, 12]}>
+    <Navbar cta={false} />
+    <Box maxW="4xl" mx="auto">
+      <Link color="green.500" fontSize="5xl" href="/courses">
+        <IoArrowBack />
+      </Link>
+      <Suspense fallback={<CourseContentSkeleton />}>
         <CourseContent slug={course} />
-        </Suspense>
-      </Box>
+      </Suspense>
     </Box>
-  );
-};
+  </Box>
+);
 
 export default CoursePage;
