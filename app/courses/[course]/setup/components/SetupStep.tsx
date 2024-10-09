d7d40e846d921113089d601915287c0e01a50f06
batman <iammasterbrucewayne@protonmail.com>
2024-10-09 18:52:00 +0530

    refactor(app/courses/[course]/setup/components/SetupStep.tsx): remove redundant code

--- app/courses/[course]/setup/components/SetupStep.tsx
+++ app/courses/[course]/setup/components/SetupStep.tsx
@@ -1,42 +1,24 @@
 import { Heading, Text } from "@chakra-ui/react";
 import { motion } from "framer-motion";
-import { ObjectId, WithId } from "mongodb";
+import { WithId } from "mongodb";
 import React, { useEffect, useState } from "react";
 
 import { Repository } from "@/lib/db/models";
-import { AnswerOptions, RepositorySetup, SetupQuestion } from "@/lib/types";
+import { AnswerOptions, SetupQuestion } from "@/lib/types";
 
 import { QuestionOptions } from "./QuestionOptions";
-import { RepositorySteps } from "./RepositoryStepBlocks";
-import { RepoStepLoadingSkeleton } from "./RepoStepLoadingSkeleton";
 
 type SetupStepProps = {
-  step: SetupQuestion | RepositorySetup;
+  step: SetupQuestion;
   onOptionClick?: (option: AnswerOptions) => void;
   isLoading?: boolean;
-  gitPushReceived?: boolean;
-  startingLessonUrl?: string;
-  repoSetupComplete?: boolean;
-  courseSlug?: string;
-  userId?: ObjectId;
-  courseId?: ObjectId;
   initialRepo?: WithId<Repository> | null;
 };
-
-const checkSetupQuestion = (
-  step: SetupQuestion | RepositorySetup,
-): step is SetupQuestion => step.kind === "setup_question";
 
 export const SetupStep: React.FC<SetupStepProps> = ({
   step,
   onOptionClick,
   isLoading,
-  gitPushReceived = false,
-  startingLessonUrl,
-  repoSetupComplete = false,
-  courseSlug,
-  userId,
-  courseId,
   initialRepo,
 }) => {
   const [repo, setRepo] = useState<WithId<Repository> | null>(
@@ -50,29 +32,6 @@ export const SetupStep: React.FC<SetupStepProps> = ({
     }
   }, [initialRepo]);
 
-  const isSetupQuestion = checkSetupQuestion(step);
-
-  const renderContent = () => {
-    if (isSetupQuestion) {
-      return (
-        <QuestionOptions onOptionClick={onOptionClick} options={step.options} />
-      );
-    }
-
-    if (isLoading || !courseSlug || !repo) {
-      return <RepoStepLoadingSkeleton />;
-    }
-
-    return (
-      <RepositorySteps
-        gitPushReceived={gitPushReceived}
-        repoSetupComplete={repoSetupComplete}
-        startingLessonUrl={startingLessonUrl}
-        steps={(step as RepositorySetup).steps}
-      />
-    );
-  };
-
   return (
     <motion.div
       animate={{ opacity: 1, y: 0 }}
@@ -82,12 +41,12 @@ export const SetupStep: React.FC<SetupStepProps> = ({
       transition={{ duration: 0.3 }}
     >
       <Heading mb={2} size="md">
-        {isSetupQuestion ? step.question : step.title}
+        {step.question}
       </Heading>
       <Text color="gray.400" mb={6}>
         {isLoading || !repo ? loadingText : step.description}
       </Text>
-      {renderContent()}
+      <QuestionOptions onOptionClick={onOptionClick} options={step.options} />
     </motion.div>
   );
 };
