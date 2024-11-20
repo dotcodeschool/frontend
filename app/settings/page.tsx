a8eeea83d8d097006df570dad258acc3f50f95ad
batman <iammasterbrucewayne@protonmail.com>
2024-11-20 11:02:29 +0700

    WIP: add navbar to settings page and redirect to login if session not found

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,14 +1,28 @@
 import { Container, Heading } from "@chakra-ui/react";
+import { redirect } from "next/navigation";
+import { auth } from "../../auth";
 
 import { NotificationPreferences } from "./components/NotificationPreferences";
+import { Navbar } from "@/components/navbar";
 
-const SettingsPage = () => (
-  <Container maxW="4xl" p={6}>
-    <Heading as="h1" mb={8} size="xl">
-      Notification Settings
-    </Heading>
-    <NotificationPreferences />
-  </Container>
-);
+export default async function SettingsPage() {
+  const session = await auth();
 
-export default SettingsPage;
+  if (!session) {
+    redirect("/api/auth/signin");
+  }
+
+  return (
+    <>
+      <Container px={6} maxW="4xl">
+        <Navbar cta={false} />
+      </Container>
+      <Container maxW="4xl" p={6}>
+        <Heading as="h1" pl={4} size="xl">
+          Notification Settings
+        </Heading>
+        <NotificationPreferences />
+      </Container>
+    </>
+  );
+}
