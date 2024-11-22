732c505b9f6d05c2056d9708767d10d3f213f746
iammasterbrucewayne <batman@dotcodeschool.com>
2024-11-22 18:52:45 +0700

    fix: issue where session was null

--- auth.ts
+++ auth.ts
@@ -1,14 +1,12 @@
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
-import NextAuth, { NextAuthConfig } from "next-auth";
-import GitHub from "next-auth/providers/github";
+import NextAuth from "next-auth";
 
-import { clientPromise } from "@/lib/db/mongodb";
+import { authConfig } from "./auth.config";
+import { clientPromise } from "./lib/db/mongodb";
 
-const adapter = MongoDBAdapter(clientPromise);
-
-export const authConfig: NextAuthConfig = {
-  providers: [GitHub],
-  adapter,
-};
-
-export const { auth, handlers, signIn, signOut } = NextAuth(authConfig);
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  ...authConfig,
+  adapter: MongoDBAdapter(clientPromise, {
+    databaseName: process.env.DB_NAME,
+  }),
+});
