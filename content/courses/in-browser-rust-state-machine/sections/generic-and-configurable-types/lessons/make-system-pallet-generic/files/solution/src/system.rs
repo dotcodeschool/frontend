87fad716c4e96add7d03604c6e47b5dca969558c
iammasterbrucewayne <batman@dotcodeschool.com>
2026-03-31 18:58:09 +0530

    chore: update tutorial content from latest gitorial sources

---
+++ content/courses/in-browser-rust-state-machine/sections/generic-and-configurable-types/lessons/make-system-pallet-generic/files/solution/src/system.rs
@@ -0,0 +1,56 @@
+use core::ops::AddAssign;
+use num::traits::{One, Zero};
+use std::collections::BTreeMap;
+
+/// This is the System Pallet.
+/// It handles low level state needed for your blockchain.
+#[derive(Debug)]
+pub struct Pallet<AccountId, BlockNumber, Nonce> {
+	/// The current block number.
+	block_number: BlockNumber,
+	/// A map from an account to their nonce.
+	nonce: BTreeMap<AccountId, Nonce>,
+}
+
+impl<AccountId, BlockNumber, Nonce> Pallet<AccountId, BlockNumber, Nonce>
+where
+	AccountId: Ord + Clone,
+	BlockNumber: Zero + One + AddAssign + Copy,
+	Nonce: One + AddAssign + Default,
+{
+	/// Create a new instance of the System Pallet.
+	pub fn new() -> Self {
+		Self { block_number: BlockNumber::zero(), nonce: BTreeMap::new() }
+	}
+
+	/// Get the current block number.
+	pub fn block_number(&self) -> BlockNumber {
+		self.block_number
+	}
+
+	// This function can be used to increment the block number.
+	// Increases the block number by one.
+	pub fn inc_block_number(&mut self) {
+		self.block_number += BlockNumber::one();
+	}
+
+	// Increment the nonce of an account. This helps us keep track of how many transactions each
+	// account has made.
+	pub fn inc_nonce(&mut self, who: &AccountId) {
+		*self.nonce.entry(who.clone()).or_default() += Nonce::one();
+	}
+}
+
+#[cfg(test)]
+mod test {
+	#[test]
+	fn init_system() {
+		let mut system = super::Pallet::<String, u32, u32>::new();
+		system.inc_block_number();
+		system.inc_nonce(&"alice".to_string());
+
+		assert_eq!(system.block_number(), 1);
+		assert_eq!(system.nonce.get("alice"), Some(&1));
+		assert_eq!(system.nonce.get("bob"), None);
+	}
+}
