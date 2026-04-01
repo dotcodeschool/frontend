3830f8d52155266ad1eb5ed7a32e9e72be60dc8a
iammasterbrucewayne <batman@dotcodeschool.com>
2026-04-01 08:44:21 +0530

    fix: remove bottom margin on last code block to avoid gap in quiz explanations

--- src/shared/components/mdx-components/CodeBlock.tsx
+++ src/shared/components/mdx-components/CodeBlock.tsx
@@ -66,7 +66,7 @@ export function CodeBlock({ children, className, filename }: Props) {
 
   return (
     <div
-      className="border border-border rounded-lg overflow-hidden mb-8 relative"
+      className="border border-border rounded-lg overflow-hidden mb-6 last:mb-0 relative"
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
