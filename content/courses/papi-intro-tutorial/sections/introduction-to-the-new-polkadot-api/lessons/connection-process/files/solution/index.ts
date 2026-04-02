9e35e92f1af3a1772500b057d248f07a7e302593
iammasterbrucewayne <batman@dotcodeschool.com>
2026-04-02 14:22:12 +0530

    remove papi test

--- content/courses/papi-intro-tutorial/sections/introduction-to-the-new-polkadot-api/lessons/connection-process/files/solution/index.ts
+++
@@ -1,9 +0,0 @@
-import { createClient, type PolkadotClient } from "polkadot-api";
-import { getWsProvider } from "polkadot-api/ws-provider/web";
-
-function makeClient(endpoint: string): PolkadotClient {
-  console.log(`Connecting to endpoint: ${endpoint}`);
-  const provider = getWsProvider(endpoint);
-  const client = createClient(provider);
-  return client;
-}
